fababc0b5140bf3aea982fa71e919492a33fb2d9
cslee15 <cslee15@DESKTOP-SVQ61N0>
2022-01-21 17:46:10 +0900

    Spring으로 게시판만들기

---
+++ board/src/main/java/com/spring/board/SampleController.java
@@ -0,0 +1,18 @@
+package com.spring.board;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/sample")
+public class SampleController {
+	
+	@RequestMapping("/getSample")
+	public void getSample(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		System.out.println("getSample");
+	} 
+	
+}
